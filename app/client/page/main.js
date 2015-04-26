66cc11012fdb58a8c652b81b799083d361ff2cb1
Anonymous <emmostrom@gmail.com>
2015-04-26 17:11:33 +0000

    Smooth scrolling to sections on main page

--- app/client/page/main.js
+++ app/client/page/main.js
@@ -3,5 +3,18 @@ Template.Main.events({
     $('#login-dropdown-list').addClass('open')
     e.preventDefault()
     return false;
+  },
+  
+  'click a[href*=#]': function(e){
+    var link = e.currentTarget
+    if (location.pathname.replace(/^\//,'') == link.pathname.replace(/^\//,'') && location.hostname == link.hostname) {
+      var target = $(link.hash);
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
   }
 })
