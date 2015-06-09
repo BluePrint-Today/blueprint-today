f33ba399531f694c67eb0e2ef037a6b32213fc8e
Anonymous <emmostrom@gmail.com>
2015-06-09 01:20:57 +0000

    Add course to student dialog

--- app/client/page/gradebook.js
+++ app/client/page/gradebook.js
@@ -1,23 +1,28 @@
 Template.Gradebook.created = function(){
-  Template.defaultProperty(this, 'data', {})
   this.studentId = new ReactiveVar(null)
 }
 
 Template.Gradebook.helpers({
-  'studentCourse': function(){
-    var studentId = Template.instance().studentId.get()
-    if(studentId){
-      var student = Student.findOne({_id: studentId})
-      return {'studentName': student.name}
-    }else{
-      return {}
-    }
+  'courses': function(){
+      var termId = Session.get('current_term')
+      return Course.find({termId: termId}, {sort: {title: 1}})
   }
 })
 
 Template.Gradebook.events({
   'click #courses_link': function(){
     Template.dialog_box.open('#course_list_manage_dialog')
+  },
+  
+  'click .dialog_box_save': function(event){
+    var studentId = Template.instance().studentId.get()
+    var titleValue = Template.instance().$('#courseTitle')[0].value.trim()
+    var selectedId = Session.get('list_panel')
+    var termId = Session.get('current_term')
+    if(titleValue.length > 0){
+      Course.save({termId: termId, title: titleValue, students: [studentId]})
+      Template.dialog_box.close('#student_add_course_dialog')
+    }
   }
 })
 
@@ -25,5 +30,5 @@ Template.Gradebook.addCourse = function(studentId){
   var page = $('.gradebook-page')[0]
   var pageTI = Blaze.getView(page).templateInstance()
   pageTI.studentId.set(studentId)
-  Template.dialog_box.open('#student_add_course_dialog')
+  Template.dialog_box.open('#student_add_course_dialog', 'add')
 }
